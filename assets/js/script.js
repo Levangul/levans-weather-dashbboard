3f36d5323a3ce19033f2141dac853857fa357ecc
Levangul <150108077+Levangul@users.noreply.github.com>
2024-01-27 02:24:13 -0500

    working on base

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+
+
+//* https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+let searchedCity = document.getElementById('cityname')
+let button = document.getElementById('button')
+let apiKey = '56cb8785bd6021e59f6df2e539cf6f2c'
+let searchHistory = []
+
+
+
+
+
+button.addEventListener("click", function (event) {
+    event.preventDefault()
+    let city = searchedCity.value
+    if (city !== "") {
+        displayWeather(city)
+    }
+    console.log(city)
+
+})
+
+
+function displayWeather(city) {
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    let cityName = document.getElementById('current-city');
+                    let temp = document.getElementById('current-city-temperature')
+                    let wind = document.getElementById('current-city-wind')
+                    let humidity = document.getElementById('current-city-humidity')
+                    temp.textContent = "Temperature: " + Math.round(data.main.temp) + " F";
+                    humidity.textContent = "Humidity: " + data.main.humidity + " %";
+                    wind.textContent = "Wind: " + data.wind.speed + " MPH";
+                    cityName.textContent = data.name
+
+                    console.log(data)
+                });
+            }
+        })
+}
+
+
+
+
+
+
